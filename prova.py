1aee27177d64ec9341002cfa747c44c367a1960f
ivanColucci <32535415+ivanColucci@users.noreply.github.com>
2020-03-12 17:54:26 +0100

    Refactor

--- prova.py
+++ prova.py
@@ -1,4 +1,5 @@
 from file_interface import read_file, write_file
+from utils import process_map, extract_developers
 import numpy as np
 from multiprocessing import Pool
 
@@ -7,42 +8,18 @@ files = ['a_solar.txt', 'b_dream.txt', 'c_soup.txt', 'd_maelstrom.txt', 'e_igloo
 # for file in files:
 #     read_file(file)
 info, companies = read_file(files[0])
-
-print(info)
-print(companies)
 map = info['map']
 
-r_index = 0
-c_index = 0
-PM = []
-DE = []
-for r in map:
-    for c in r:
-        coor = (r_index, c_index)
-        if c == 'M':
-            PM.append(coor)
-        if c == '_':
-            DE.append(coor)
-        c_index += 1
-    r_index += 1
-
-print(PM)
-print(DE)
+PM, DE = process_map(map)
 
 pms = info['pms']
 pms.sort(key=lambda x:(companies[x['company']], x['bonus']), reverse=True)
-print(pms)
-
 developers = info['developers']
 developers.sort(key=lambda x:x['n_skills'], reverse=True)
-print(developers)
 
-dev_companies = {}
-for comp in companies.keys():
-    for dev in developers:
-        if dev['company'] == comp:
-            if comp not in dev_companies:
-                dev_companies[comp] = []
-            dev_companies[comp].append(dev)
+dev_companies = extract_developers(developers, companies)
 
-print(dev_companies)
+output = {}
+output['developers'] = info['developers']
+output['pms'] = info['pms']
+# write_file(files[0].split('.txt')[0]+"_out.txt", output)
